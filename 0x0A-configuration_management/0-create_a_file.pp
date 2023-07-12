d6b8254327892cfe576309db494b60443307a12b
rky-ke <ronaldyegon15@gmail.com>
2023-07-12 06:41:13 -0700

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #creates a file in /tmp
-
 file { 'school' :
+  path    => '/tmp/school',
+  ensure  => 'file',
   content => 'I love puppet',
   group   => 'www-data',
   mode    => '0744',
-  owner   => 'www-data',
-  path    => '/tmp/school'
+  owner   => 'www-data'
 }
